e2d09d4c746595cc6bd49ecda87666b65c73c2c2
AlejandroSugeno <Alexityco@gmail.com>
2022-05-06 10:31:33 -0400

    tercer commit

--- src/App.js
+++ src/App.js
@@ -40,7 +40,24 @@ function App() {
         </div>
     </div>
 
-  
+    <div class="card">
+        <div class="card_header blue">
+            <div class="tittle">
+                <div class="circle">1</div>
+                <p>Agenda/Topic</p>
+            </div>
+            <div class="id">
+                <p>SAAS-000</p>
+            </div>
+        </div>
+        <div class="card_section">
+            <p>Descripcion de la card</p>
+        </div>
+        <div class="card_footer">
+            <p>Mis Actualizaciones</p>
+            <p>14/07/2021</p>
+        </div>
+    </div></>
 
   );
 }
